215e38b5715d1ff23b25d76c29ab45197d7b3113
Binoy Philip <bp-prv@umich.edu>
2013-05-06 14:37:14 -0400

    Proto test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UmsnProto::Application.config.secret_token = '2dba5dee66a2ab63c9e5f7e0790ecfcf2e3de073b62b18a8e39c509e855d81112314c8ca325019106347e27a76df26ea231bafcd67ab9cb85b03fcc5c9a51220'
